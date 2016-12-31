dc9d576bcb3bc477dbeb7e38ac94ca3db014e801
Jonas Große Sundrup <cherti@letopolis.de>
2017-01-01 00:45:34 +0100

    implement weeks and headlines

--- tcal.py
+++ tcal.py
@@ -2,12 +2,30 @@
 
 import datetime, calendar
 
+print_weeks = True
 m = 12
+appointments = {}
 
 def print_month(y, m):
 	cal = calendar.Calendar()
-	print("Mo Di Mi Do Fr Sa So")
-	line = ""
+	d = datetime.date(y, m, 1)
+	monthyearline = d.strftime("%B {}".format(y))
+	weekdaysline = "Mo Di Mi Do Fr Sa So"
+	if print_weeks:
+		weekdaysline = "    " + weekdaysline
+		# get week number
+		monthstart = datetime.date(y, m, 1)
+		weekno = monthstart.isocalendar()[1]
+		line = "{:2}  ".format(weekno)
+	else:
+		line = ""
+		weekno = None
+
+	offset_monthyearline = max(int((len(weekdaysline) - len(monthyearline))/2), 0)
+	print(offset_monthyearline*" " + monthyearline)
+
+	print(weekdaysline)
+
 	linelength = 0
 	for day, weekday in cal.itermonthdays2(y, m):
 		if day == 0:
@@ -19,10 +37,15 @@ def print_month(y, m):
 		if linelength == 7:
 			print(line)
 			linelength = 0
-			line = ""
+			if weekno:
+				weekno += 1
+				line = "{:2}  ".format(weekno)
+			else:
+				line = ""
 
 
 
 
 if __name__ == "__main__":
 	print_month(2016, 12)
+
