d6056122045b8d08a41e52a285b859bb6451c806
Jonas Große Sundrup <cherti@letopolis.de>
2017-01-01 22:54:42 +0100

    refactor all the things

--- tcal.py
+++ tcal.py
@@ -8,6 +8,7 @@ appointments = {}
 appointmentdaycolor = 'cyan'
 
 today = datetime.date.today()
+cal = calendar.Calendar()
 
 def errprint(msg, code):
 	print(msg, file=sys.stderr)
@@ -24,57 +25,63 @@ group = parser.add_mutually_exclusive_group(required=False)
 group.add_argument('-n', '--new', action='store_true', default=False, dest='new', help='add new appointment')
 group.add_argument('-e', '--edit', action='store_true', default=False, dest='edit', help='edit appointments for specific date')
 
-#args = parser.parse_args(sys.argv[1:])
 args = parser.parse_args()
 
 
+# templates for display mechanism
+weekdaysline = " Mo  Di  Mi  Do  Fr  Sa  So"
+blank_day = " "*4
+date_id = lambda y, m, d: "{}-{:02}-{:02}".format(y, m, d)
+fmt_day_col = lambda day: " {} ".format(colored("{:2}".format(day), appointmentdaycolor))
+fmt_day = lambda day: " {:2} ".format(day)
+fmt_monthyear = lambda y, m: datetime.date(y, m, 1).strftime("%B {}".format(y))
+fmt_week_prefix = lambda weekno: "{:2}  ".format(weekno)
+
+# compute the offset as half of the whitespaces the weekdaysline is longer than the monthyearline
+centering_whitespaces = lambda len_centered_string, len_full_line_string: max(int((len_full_line_string - len_centered_string)/2), 0)*" "
+
 
 def print_month(y, m):
-	cal = calendar.Calendar()
-	month_appointment_identifier = []
+	month_appointment_identifier = []    # collecting the appointments to be printed below this month
+	monthyearline = fmt_monthyear(y, m)  # headerline of the month
+	global weekdaysline                  # use weekdaysline that was defined in the templates
 
-
-	d = datetime.date(y, m, 1)
-	monthyearline = d.strftime("%B {}".format(y))
-	weekdaysline = "Mo Di Mi Do Fr Sa So"
 	if args.print_weeks:
-		weekdaysline = "    " + weekdaysline
-		# get week number
-		monthstart = datetime.date(y, m, 1)
-		weekno = monthstart.isocalendar()[1]
-		line = "{:2}  ".format(weekno)
+		# prepare prefix for the weeks
+		weekdaysline = len(fmt_week_prefix(42))*" " + weekdaysline  # extend weekdaysline to be properly aligned with weeknumbers in front
+		_, isoweek, _ = datetime.date(y, m, 1).isocalendar()  # returns year, weekno and dayno of the year, we want the first weekno relevant
+		line = fmt_week_prefix(isoweek)
 	else:
 		line = ""
-		weekno = None
+		isoweek = None
 
-	# compute the offset as half of the whitespaces the weekdaysline is longer than the monthyearline
-	offset_monthyearline = max(int((len(weekdaysline) - len(monthyearline))/2), 0)
-	print(offset_monthyearline*" " + monthyearline)
-
+	print(centering_whitespaces(len(monthyearline), len(weekdaysline)) + monthyearline)
 	print(weekdaysline)
 
-	linelength = 0
+
+	lineblocks = 0
 	for day, weekday in cal.itermonthdays2(y, m):
 		if day == 0:
-			line += " "*4
+			line += blank_day
 		else:
-			identifier = "{}-{:02}-{:02}".format(y, m, day)
+			identifier = date_id(y, m, day)
 			if identifier in appointments:
 				month_appointment_identifier.append(identifier)  # remember that identifier to print appointments in the end
-				line += " {} ".format(colored("{:2}".format(day), appointmentdaycolor))  # color that day
+				line += fmt_day_col(day)  # color that day
 			else:
-				line += " {:2} ".format(day)  # print without color
+				line += fmt_day(day) # print without color
 
 
 
-		linelength += 1
-		if linelength == 7:
+		lineblocks += 1
+		if lineblocks == 7:  # line is full, week is complete
 			print(line)
-			linelength = 0
+			lineblocks = 0
 
-			if weekno:
-				weekno = (weekno + 1)%52
-				line = "{:2}  ".format(weekno)
+			# reinitialize line with or without week-prefix
+			if isoweek:
+				isoweek = (isoweek + 1)%52
+				line = fmt_week_prefix(isoweek)
 			else:
 				line = ""
 	print()
@@ -103,14 +110,17 @@ def load_appointments(filepath):
 
 def create_appointment(y, m, d):
 
-	y = int(input("year[{}]:  ".format(y)) or y)
-	m = int(input("month[{}]: ".format(m)) or m)
-	d = int(input("day[{}]:   ".format(d)) or d)
+	try:
+		y = int(input(    "year[{}]: ".format(y)) or y)
+		m = int(input(" month[{:2}]: ".format(m)) or m)
+		d = int(input("   day[{:2}]: ".format(d)) or d)
+		desc = input("Appointment description: ")
 
-	desc = input("Appointment description: ")
+		with open(args.appointment_file, 'a') as f:
+			print("{}-{:02}-{:02} {}".format(y, m, d, desc), file=f)
 
-	with open(args.appointment_file, 'a') as f:
-		print("{}-{:02}-{:02} {}".format(y, m, d, desc), file=f)
+	except KeyboardInterrupt:
+		print("\nTermination by user, no appointment has been added")
 
 
 if __name__ == "__main__":
@@ -119,17 +129,24 @@ if __name__ == "__main__":
 
 	load_appointments(args.appointment_file)
 
-	if not args.edit and not args.new:
+	if not args.edit and not args.new:  # the default, which is just printing the current state of appointments
 		print_month(args.year, args.month)
-		if args.monthrange > 1:
-			d = datetime.date(args.year, args.month, 1)
 
+		if args.monthrange > 1:
+			# we need a date we can increment by one month and still be guaranteed to land in a correct next month
+			# promlematic are 31sts in general, the ends of January, because February is especially short, etc.
+			# to avoid all that hassle, we just take the first of our currently selected month, this way we get
+			# rid of all the hassle and can use that for computation of the next months
+			# Why not incrementing years and month manually? because this way yearbreaks and stuff are handled
+			# by python-datetime, which might be overhead, but is sufficiently cheap to avoid cornercases by relying
+			# on a reference implementation that has already dealt with possible date-cornercases
+			dummydate = datetime.date(args.year, args.month, 1)
 			for i in range(args.monthrange):
-				d = d + relativedelta(months=1)
-				print_month(d.year, d.month)
+				dummydate = dummydate + relativedelta(months=1)
+				print_month(dummydate.year, dummydate.month)
 
 	elif args.new:
 		create_appointment(today.year, today.month, today.day)
-	elif args.edit:
-		print("ahm, editing is not yet supported")
 
+	elif args.edit:
+		print("ahm, editing is not yet supported. Should be implemented soon-ish.")
